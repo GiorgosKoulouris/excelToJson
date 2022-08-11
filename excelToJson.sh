39286ad8cf9932ef23cc452a206fb78db16b7db6
Giorgos Koulouris <grgkoulouris@yahoo.gr>
2022-08-11 04:11:17 +0300

    Added input validation and file-argument iteration in main script

--- excelToJson.sh
+++ excelToJson.sh
@@ -1,24 +1,64 @@
 #!/bin/bash
 
-# Get script parent Directory so you can locate its helper file
-scriptPath=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+canExecute=true
 
-xlsPath=$1
-xlsParentDir="$(dirname "$xlsPath")"
+# Check that there is at least one argument
+if [ "$#" -eq 0 ];
+then
+    echo "You need at least one XLS or XLSX file as an argument. Usage example:"
+    echo ""
+    echo "  /path/to/excelToJason.sh /path1/to1/excel1.xls /path2/to2/excel2.xls"
+    canExecute=false
+fi
 
-# Delete Later
-source /Users/louris/Dev_Projects/Coding/Scripts/excelToJson/venv/bin/activate
+# Check that files exist and are in a valid format
+for file in "$@"
+do
+    if [ ! -f "$file" ]; then
+        echo "File doesn't exist:"
+        echo ""
+        echo "  ${file}"
+        canExecute=false
+    fi
 
-# venvPath="${scriptPath}/tempVenv$RANDOM"
-# python3 -m venv $venvPath
-# source "${venvPath}/bin/activate"
-# python3 -m pip install --upgrade pip
-# python3 -m pip install pandas
-# python3 -m pip install openpyxl
+    filename=$(basename -- "$file")
+    extension="${filename##*.}"
+    if [ $extension != "xls" ] && [ $extension != "xlsx" ]
+    then
+        echo "File doesn't seem to be an excel file (.xls or .xlsx)"
+        echo ""
+        echo "  ${file}"
+        canExecute=false
+    fi
+done
 
-command="${scriptPath}/excelToJsonHelper.py ${xlsPath}"
-python3 $command
+if $canExecute = true ; then
 
-deactivate
+    # Get script parent Directory so you can locate its helper file
+    scriptPath=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# rm -rf $venvPath
+
+    # # Main Scenario
+    # venvPath="${scriptPath}/tempVenv$RANDOM"
+    # python3 -m venv $venvPath
+    # source "${venvPath}/bin/activate"
+    # python3 -m pip install --upgrade pip
+    # python3 -m pip install pandas
+    # python3 -m pip install openpyxl
+
+    for xlsPath in "$@"
+    do
+        command="${scriptPath}/excelToJsonHelper.py ${xlsPath}"
+        python3 $command
+    done
+
+    deactivate
+
+    # # Main Scenario
+    # rm -rf $venvPath
+
+else
+    echo ""
+    echo "Quiting...."
+fi
+
